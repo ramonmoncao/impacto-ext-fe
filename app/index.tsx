e5fb9462a63f18ddb0b26bd6a7296ff1a2dfbe04
Erik Gomes <gomeserik35@gmail.com>
2026-04-20 10:54:42 -0300

    fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  TextInput, 
-  TouchableOpacity, 
-  Image, 
-  SafeAreaView, 
-  Platform 
-} from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <Text style={{ color: '#060606', fontSize: 24, fontWeight: 'bold' }}>
-      Hello, World!
-    </Text>
+    <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "#060606", fontSize: 24, fontWeight: "bold" }}>
+        Hello, World!
+      </Text>
+    </SafeAreaView>
   );
 }
