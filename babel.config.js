4a3b95f3e600ef3fa6ef6b05210acfe3e978778f
Erik Gomes <gomeserik35@gmail.com>
2026-06-02 15:39:57 -0300

    update babel.config

--- babel.config.js
+++ babel.config.js
@@ -5,8 +5,6 @@ module.exports = function (api) {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
-    plugins: [
-      'react-native-reanimated/plugin', // O Reanimated sozinho já é suficiente e DEVE ser o último
-    ],
+    
   };
 };
